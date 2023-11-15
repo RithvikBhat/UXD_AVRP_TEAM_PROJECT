25ddb7b2979aa8ab6d934dc5482684778fef941a
RithvikBhat <code.rithvikkbhat@gmail.com>
2023-11-15 15:12:29 +0100

    inserted map and duo character, cafe items

---
+++ Assets/Position.cs
@@ -0,0 +1,24 @@
+using HCIG;
+using Leap;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+
+    private GameObject cursor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(BaseManager.Instance.Camera.transform.position.x, 0, BaseManager.Instance.Camera.transform.position.z);
+    }
+}
